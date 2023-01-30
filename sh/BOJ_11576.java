66b99734934fff1d07efe0ffef433745407d127b
소수현 <ssuheyon98@gmail.com>
2023-01-30 23:21:50 +0900

    [BOJ] / Base Conversion / 실버5 / 30분

--- sh/BOJ_11576.java
+++ sh/BOJ_11576.java
@@ -6,7 +6,6 @@ import java.util.List;
 import java.util.StringTokenizer;
 
 public class BOJ_11576 {
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
