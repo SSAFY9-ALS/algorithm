148938e801f90e7138ec7975dc825d87914e7028
tngus2sh <101691649+tngus2sh@users.noreply.github.com>
2023-02-01 22:45:37 +0900

    Revert "data structure"

---
+++ dh/BOJ_17087.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		public static int gcd(int p, int q) {
+			// 최대공약수 구하기 (유클리드 호제법)
+			if (q == 0)
+				return p;
+			return gcd(q, p % q);
+		 }
+		
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int S = sc.nextInt();
+		
+		int[] arr = new int[N];
+		for(int i = 0; i<N;i++) {
+			int temp = sc.nextInt();
+			arr[i] = Math.abs(temp - S);
+			// 수빈이와 동생의 거리 저장
+		}
+		
+		int answer = arr[0];
+		for(int i = 1; i < N; i++) {
+			answer = gcd(answer, arr[i]);
+			// 거리들의 최대 공약수 구하기
+		}
+		
+		System.out.println(answer);
+	}
+
+}
