ecfecef5ffe472841dd20c8f11d398665026ec26
qhfhdls <79788971+DongHyun22@users.noreply.github.com>
2023-02-03 03:09:36 +0900

    [BOJ] 소트인사이드 / 실버5 / 8분

---
+++ dh/BOJ_1427.java
@@ -0,0 +1,23 @@
+package algorithm;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String s = sc.nextLine();
+		Integer[] arr = new Integer[s.length()];
+		for (int i = 0; i < s.length(); i++) {
+			arr[i] = s.charAt(i) - '0';
+		}
+		Arrays.sort(arr, Collections.reverseOrder());
+		// 역순 정렬
+		for (int i = 0; i < s.length(); i++) {
+			System.out.print(arr[i]);
+		}
+
+	}
+}
+// https://www.acmicpc.net/problem/1427
