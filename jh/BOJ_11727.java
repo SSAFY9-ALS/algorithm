3c2a184b5d6f4114258ec4cd3683c60e2befba88
JH <ljh8190@naver.com>
2023-02-06 23:52:08 +0900

    [BOJ] 2xn 타일링 2 / 실버3 / 15분 / 2월 6일

--- jh/BOJ_11727.java
+++ jh/BOJ_11727.java
@@ -10,10 +10,10 @@ import java.util.Scanner;
 public class BOJ_11727 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
+		int n = sc.nextInt(); // n 입력
 		int[] dp = new int[n+1];
-		dp[1] = 1;
-		dp[2] = 3;
+		dp[1] = 1; // 초기값 설정
+		dp[2] = 3; // 초기값 설정
 		for(int i = 3; i < dp.length; i++) {
 			// 2xn 블록을 채우는 방법은 (n-1)까지 채운 상태에서 2x1 블록을 세로로 채우는 것과 (n-2)까지 채운 상태에서 2x1 블록 2개를 가로로 겹쳐서 쌓는 방법, 2x2 블록 하나를 놓는 방법이 있음
 			dp[i] = (2 * dp[i-2] + dp[i-1]) % 10007;
