2b311f71f91e0e1a0f9d6097bbd0b93227f3b8b2
qhfhdls <79788971+DongHyun22@users.noreply.github.com>
2023-02-11 18:23:30 +0900

    [BOJ] 중복빼고정렬하기 / 실버5 / 10분 / 2월 11일

--- dh/BOJ_10867.java
+++ dh/BOJ_10867.java
@@ -7,6 +7,13 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+/**
+ * 
+ * 중복빼고정렬하기 / 실버5 / 10분
+ * https://www.acmicpc.net/problem/10867
+ *
+ */
+
 public class Main {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -15,9 +22,11 @@ public class Main {
 		String[] s = br.readLine().split(" ");
 		for(int i = 0; i < n; i++) {
 			set.add(Integer.parseInt(s[i]));
+			// 중복 제거를 위해 set에 값 저장
 		}
 		List<Integer> list = new ArrayList<>(set);
 		Collections.sort(list);
+		// 정렬을 위해 set을 list로 변환
 		
 		for(int i : list)
 			System.out.print(i + " ");
